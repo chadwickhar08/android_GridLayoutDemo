5805fbebee78bf19e77b0881beefa63d165c9a0d
Chad Harris <chadwickhar08@gmail.com>
2017-12-06 16:21:06 -0500

    Modified code to play audio files.

--- app/src/main/java/com/chadmichaelh/gridlayoutdemo/MainActivity.java
+++ app/src/main/java/com/chadmichaelh/gridlayoutdemo/MainActivity.java
@@ -1,13 +1,36 @@
 package com.chadmichaelh.gridlayoutdemo;
 
+import android.media.MediaPlayer;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    public void onClick(View view)
+    {
+        int id = view.getId();
+        String ourId = "";
+
+        ourId = view.getResources().getResourceEntryName(id);
+
+        int resourceId = getResources().getIdentifier(ourId, "raw", "com.chadmichaelh.gridlayoutdemo");
+
+        MediaPlayer mplayer = MediaPlayer.create(this, resourceId);
+
+        mplayer.start();
+
+        Log.i("button tapped", Integer.toString(resourceId));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
